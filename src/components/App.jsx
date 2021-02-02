3dd8cf9f452767f96ff6731db0c41576f61a219b
goyaladiti997 <aditi.goyal.btech2018@sitpune.edu.in>
2021-02-02 23:22:16 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Entry from "./Entry";
+
+function CreateEntry(entry){
+  return(
+    <Entry
+      key={entry.id}
+      emoji={entry.emoji}
+      name={entry.name}
+      meaning={entry.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>Emojipedia</span>
+      </h1>      
+      <dl className="dictionary">
+      {emojipedia.map(CreateEntry)}        
+      </dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,42 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😏",
+    name: "Smirking/Whimsical Face",
+    meaning: "Half a smile that oozes with complacency. Represents irony, coolness or playfulness. Be careful: Somebody could be up to something. Used as an offensive look for flirting or sexual innuendo."
+  },
+  {
+    id: 5,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning: "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness."
+  },
+  {
+    id: 6,
+    emoji: "🤭",
+    name: "Face With Hand Over The Mouth",
+    meaning: "Expresses shock, surprise, or a sudden perception. Typical gesture, which shows that the other person is not telling the truth. Thinks he's being lied to."
+  }
+];
+export default emojipedia;
